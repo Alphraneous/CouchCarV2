5afef73d91fb0aeef73f6fa4e4f6c82d5e45f091
Alpharaneous <alpharaneous@ajaxcore.com>
2025-12-04 20:56:45 -0600

    First Formatting Change; not complete

--- couchCarStarterCode.ino
+++ couchCarStarterCode.ino
@@ -1,16 +1,51 @@
 #include <XBOXONE.h>
 
-// Satisfy the IDE, which needs to see the include statment in the ino too.
-#ifdef dobogusinclude
-#include <spi4teensy3.h>
+// Satisfy the IDE for includes, but do add to actual compilation target
+#ifdef _USING_ARDUINO_IDE
+#   include <spi4teensy3.h>
 #endif
 #include <SPI.h>
 
-USB Usb;
-XBOXONE Xbox(&Usb);
+#include <utility>
 
-//Set this value to true if you want to see the positions of the buttons and joysticks
-bool debug = false;
+// Set this value to true if you want to see the positions of the buttons and joysticks
+#define _ENABLE_MODE_DEBUG true
+
+namespace USBLibrary {
+    extern USB     usbDriver  {};
+    extern XBOXONE xboxDriver {&usbDriver};
+}
+
+struct XboxInputScheme {
+
+    XboxInputScheme() {
+        leftHat = { 
+            USBLibrary::xboxDriver.getAnalogHat(AnalogHatEnum::LeftHatX),
+            USBLibrary::xboxDriver.getAnalogHat(AnalogHatEnum::LeftHatY),
+        };
+
+        rightHat = {
+            USBLibrary::xboxDriver.getAnalogHat(AnalogHatEnum::RightHatX),
+            USBLibrary::xboxDriver.getAnalogHat(AnalogHatEnum::RightHatY),
+        };
+
+        leftTrigger = USBLibrary::xboxDriver.getButtonPress(AnalogHatEnum::LT);
+        rightTrigger = USBLibrary::xboxDriver.getButtonPress(AnalogHatEnum::RT);
+    }
+
+    std::pair<int16_t, int16_t> leftHat;
+    std::pair<int16_t, int16_t> rightHat;
+    
+    uint16_t leftTrigger;
+    uint16_t rightTrigger;
+
+    bool xToggle;
+    bool yToggle;
+    bool aToggle;
+    bool bToggle;
+    bool leftBbumperToggle;
+    bool rightBumperToggle;
+};
 
 //Toggle variables
 bool toggleA = false;
@@ -29,28 +64,33 @@ bool BrakeLights = false;
 bool TurnL = false;
 bool TurnR = false;
 
+// Runs once on startup
+void setup() 
+{
+    constexpr uint16_t baudeRate = 115200;
+    Serial.begin(baudeRate);
+    
+    while(!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+    
+    if(USBLibrary::usbDriver.Init() == -1) {
+        Serial.print(F("\r\nOSC did not start"));
+        while(1); // Halt
+    }
 
-//DON'T MESS WITH THIS CODE!!
-//Runs once on startup
-void setup() {
-  Serial.begin(115200);
-  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
-  if (Usb.Init() == -1) {
-    Serial.print(F("\r\nOSC did not start"));
-    while (1); //halt
-  }
-  Serial.print(F("\r\nXBOX ONE USB Library Started"));
+    Serial.print(F("\r\nXBOX ONE USB Library Started"));
 }
 
-//Runs forever
-void loop() {
-  Usb.Task();
-  if (Xbox.XboxOneConnected) {
-    int16_t LHX = Xbox.getAnalogHat(LeftHatX); //Integer from -32768 to 32767
-    int16_t LHY = Xbox.getAnalogHat(LeftHatY); //Integer from -32768 to 32767
+void loop() 
+{
+    USBLibrary::usbDriver.Task();
+    if!(USBLibrary::xboxDriver.XboxOneConnected)
+        return;
 
-    int16_t RHX = Xbox.getAnalogHat(RightHatX); //Integer from -32768 to 32767
-    int16_t RHY = Xbox.getAnalogHat(RightHatY); //Integer from -32768 to 32767
+    int16_t LHX = USBLibrary::xboxDriver.getAnalogHat(LeftHatX); //Integer from -32768 to 32767
+    int16_t LHY = USBLibrary::xboxDriver.getAnalogHat(LeftHatY); //Integer from -32768 to 32767
+
+    int16_t RHX = USBLibrary::xboxDriver.getAnalogHat(RightHatX); //Integer from -32768 to 32767
+    int16_t RHY = USBLibrary::xboxDriver.getAnalogHat(RightHatY); //Integer from -32768 to 32767
 
     /*
     Important note for the joysticks: 
@@ -60,35 +100,35 @@ void loop() {
     Make sure to keep this in mind as you're developing your control code.
     */
 
-    uint16_t LTR = Xbox.getButtonPress(LT); //Integer from 0 to 1023
-    uint16_t RTR = Xbox.getButtonPress(RT); //Integer from 0 to 1023
-    
+    uint16_t LTR = USBLibrary::xboxDriver.getButtonPress(LT); //Integer from 0 to 1023
+    uint16_t RTR = USBLibrary::xboxDriver.getButtonPress(RT); //Integer from 0 to 1023
+
     //The letter buttons are already mapped to toggle variables
     //Pressing any of these buttons toggles their corresponding variable between false and true (off and on)
     //Useful for toggling lights, etc
 
-    if (Xbox.getButtonClick(A)) {
-      toggleA = !toggleA;
+    if (USBLibrary::xboxDriver.getButtonClick(A)) {
+        toggleA = !toggleA;
     }
-    if (Xbox.getButtonClick(B)) {
-      toggleB = !toggleB;
+    if (USBLibrary::xboxDriver.getButtonClick(B)) {
+        toggleB = !toggleB;
     }
-    if (Xbox.getButtonClick(X)) {
-      toggleX = !toggleX;
+    if (USBLibrary::xboxDriver.getButtonClick(X)) {
+        toggleX = !toggleX;
     }
-    if (Xbox.getButtonClick(Y)) {
-      toggleY = !toggleY;
+    if (USBLibrary::xboxDriver.getButtonClick(Y)) {
+        toggleY = !toggleY;
     }
 
     //Unmapped buttons (LeftButton and RightButton)
     //Turn signals maybe?
     //If so, you need to use toggle variables similar to the letter buttons but make sure that only one turn signal is on at a time
     //Also, you need to 
-    if (Xbox.getButtonClick(LB)) {
+    if (USBLibrary::xboxDriver.getButtonClick(LB)) {
 
     }
-      
-    if (Xbox.getButtonClick(RB)) {
+        
+    if (USBLibrary::xboxDriver.getButtonClick(RB)) {
 
     }
 
@@ -109,31 +149,27 @@ void loop() {
     TurnL = false;        //true or false
     TurnR = false;        //true or false
 
+    delay(2);
 
 
-    //DO NOT EDIT BELOW THIS LINE!!
-    //THIS STUFF IS FOR DEBUGGING
-    if(debug) {
-      Serial.print(F("LX: "));
-      Serial.print(LHX);
-      Serial.print(F(" RX: "));
-      Serial.print(RHX);
-      Serial.print(F(" LT: "));
-      Serial.print(LTR);
-      Serial.print(F(" RT: "));
-      Serial.print(RTR);
-      Serial.print(F(" RT: "));
-      Serial.print(RTR);
-      Serial.print(F(" tY:"));
-      Serial.print(toggleY);
-      Serial.print(F(" tB:"));
-      Serial.print(toggleB);
-      Serial.print(F(" tA:"));
-      Serial.print(toggleA);
-      Serial.print(F(" tX:"));
-      Serial.print(toggleX);
-      Serial.println();
-    }
-  }
-  delay(2);
+    // Print debug information
+#if _ENABLE_MODE_DEBUG
+    Serial.print(F("LX: "));
+    Serial.print(LHX);
+    Serial.print(F(" RX: "));
+    Serial.print(RHX);
+    Serial.print(F(" LT: "));
+    Serial.print(LTR);
+    Serial.print(F(" RT: "));
+    Serial.print(RTR);
+    Serial.print(F(" toggle Y: "));
+    Serial.print(toggleY);
+    Serial.print(F(" toggle B: "));
+    Serial.print(toggleB);
+    Serial.print(F(" toggle A: "));
+    Serial.print(toggleA);
+    Serial.print(F(" toggle X: "));
+    Serial.print(toggleX);
+    Serial.println();
+#endif 
 }
